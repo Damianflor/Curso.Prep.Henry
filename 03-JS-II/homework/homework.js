cc62c9d93d4af14a8686d0969b3d1585291ad90c
damianflor <wherksted@gmail.com>
2021-10-17 13:30:51 -0500

    listo

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -102,7 +102,7 @@ function esEntero(numero) {
   // De lo contrario, devuelve "false"
   // Pista: Puedes resolver esto usando `Math.floor`
   // Tu código:
-  if (numero === Math.floor(numero, -1)){
+  if (numero === Math.floor(numero)){
     return true;
   }
   else {
